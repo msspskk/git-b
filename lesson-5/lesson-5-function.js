f9ced7745a3c5e46a0782c96db55745d5bd687b9
maska <sdfdsfds@gmail.com>
2024-02-09 21:13:46 +0200

    rjyklu

--- lesson-5/lesson-5-function.js
+++ lesson-5/lesson-5-function.js
@@ -12,23 +12,4 @@ return includedItems
 
 export default { filterExcludedItems };
 
-//2
-function calculation(cartAmount, numberOfProducts, promocode){
 
-let finalSum = 0;
-if (promocode === 'ДАРИМ300') {
-  finalSum = Math.round(cartAmount - 300)
-}
-if (numberOfProducts >= 10){
- finalSum = Math.round(cartAmount-(cartAmount * 0.05))
-}
-if (cartAmount > 50000) {
-   finalSum = Math.round(cartAmount - ((cartAmount - 50000) * 0.2))
-}
-if (cartAmount >= 20000) {
- finalSum = Math.round(cartAmount-(cartAmount * 0.15))
-}
-return finalSum;
-}
-
-export default { calculation };
--- lesson-5/lesson-5-task.js
+++ lesson-5/lesson-5-task.js
@@ -10,7 +10,24 @@ console.log (result1)
 
 
 //2
-import calculation from './lesson-5-function'
+function calculation(cartAmount, numberOfProducts, promocode){
+
+    let finalSum = 0;
+    if (promocode === 'ДАРИМ300') {
+      finalSum = Math.round(cartAmount - 300)
+    }
+    if (numberOfProducts >= 10){
+     finalSum = Math.round(cartAmount-(cartAmount * 0.05))
+    }
+    if (cartAmount > 50000) {
+       finalSum = Math.round(cartAmount - ((cartAmount - 50000) * 0.2))
+    }
+    if (cartAmount >= 20000) {
+     finalSum = Math.round(cartAmount-(cartAmount * 0.15))
+    }
+    return finalSum;
+    }
+    
 
 let cartAmount = 1000;
 let numberOfProducts = 5;
